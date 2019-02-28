3f953c892ad09c7e067f88f37dde99e9a51475b0
kmason1980 <kmason@titanfundraisingllc.com>
2019-02-27 20:29:41 -0600

    File imported that's it

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,3 +6,15 @@ Created on Sat Feb 23 14:18:45 2019
 @author: kerimason
 """
 
+import os
+import csv
+
+fileBudget_Data = open('budget_data.csv', 'r')
+reader = csv.reader(fileBudget_Data)
+csv_data = []
+for row in reader:
+    csv_data.append(row)
+fileBudget_Data.close()
+print(csv_data)
+
+
